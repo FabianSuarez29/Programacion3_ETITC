eafccfbdfed0dfd3220ae4c40131127a3e56375b
Fabian11037 <fesuareze@itc.edu.co>
2024-03-31 23:43:40 -0500

    Agregadas funciones JS
    Agregadas funciones JS

--- js/script.js
+++ js/script.js
@@ -4,5 +4,20 @@ var dia = hoy.getDate();
 var mes = hoy.getMonth();
 var anio = hoy.getFullYear();
 var el = document.getElementById('fecha');
-el.innerHTML = dia + '/' + (mes+1) + '/' + anio;
+el.innerHTML = dia + '/' + (mes+1) + '/' + anio; 
 
+/*DOM Evento al dar clic en botón URL */
+function url() {
+    let urlActual = window.location.href;
+    alert(`Esta es la URL: ${urlActual}`);
+}
+
+//Suma de 0 a n
+function sumar() {
+    const numero = parseInt(document.getElementById("numero").value);
+    let suma = 0;
+    for (let i = 0; i <= numero; i++) {
+      suma += i;
+    }
+    document.getElementById("resultado").textContent = `La suma de 0 hasta ${numero} es: ${suma}`;
+}
